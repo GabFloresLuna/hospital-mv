c5e845cb6d352bc8bcdb92c3d635d536e59576a2
GabFloresLuna <gab.floresl@duocuc.cl>
2026-04-09 12:35:03 -0400

    inicio de proyecto 2.1.5

---
+++ src/main/java/cl/duoc/hospital_vm/model/Paciente.java
@@ -0,0 +1,40 @@
+package cl.duoc.hospital_vm.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="paciente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique=true, length=13, nullable=false)
+    private String run;
+
+    @Column(nullable=false)
+    private String nombre;
+
+    @Column(nullable=false)
+    private String apeliido;
+
+    @Column(nullable=true)
+    private Date fechaNacimiento;
+
+    @Column(nullable=false)
+    private String correo;
+}
